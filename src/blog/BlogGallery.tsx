a8da5ea48e412a5aeef96a6a3e06168e10f36b73
MBC <mbc0481@naver.com>
2021-03-29 05:57:02 +0900

    message

---
+++ src/blog/BlogGallery.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+import { format } from 'date-fns';
+import Link from 'next/link';
+
+import { Pagination, IPaginationProps } from '../pagination/Pagination';
+import { PostItems } from '../utils/Content';
+import { fontSize, fontWeight } from '../utils/StyleTheme';
+
+export type IBlogGalleryProps = {
+  posts: PostItems[];
+  pagination: IPaginationProps;
+};
+
+const Layout = styled.div(() => ({
+  padding: '80px 40px',
+}));
+
+const BlogGallery = (props: IBlogGalleryProps) => (
+  <Layout>
+    <span style={{
+      fontSize: fontSize.h1,
+      fontWeight: fontWeight.bold
+    }}
+    >
+      Post
+    </span>
+    <ul>
+      {props.posts.map((elt) => (
+        <li key={elt.slug} className="mb-3 flex justify-between">
+          <Link href="/posts/[slug]" as={`/posts/${elt.slug}`}>
+            <a>
+              <h2>{elt.title}</h2>
+            </a>
+          </Link>
+
+          <div>{format(new Date(elt.date), 'LLL d, yyyy')}</div>
+        </li>
+      ))}
+    </ul>
+
+    <Pagination previous={props.pagination.previous} next={props.pagination.next} />
+  </Layout>
+);
+
+export { BlogGallery };
