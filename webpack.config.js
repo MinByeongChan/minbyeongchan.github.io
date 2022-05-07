1c54e267fde37a92639aa442fa7aac8160159a4d
MinByeongChan <mbc0481@naver.com>
2022-05-07 16:47:40 +0900

    feat(blog2022) webpack 설정 dotenv 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,62 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-console.log('REACT_APP_ENV', process.env.REACT_APP_ENV);
+module.exports = (env) => {
+  dotenv.config();
+  const { ENV } = process.env;
 
-module.exports = {
-  mode: process.env.REACT_APP_ENV,
-  entry: './src/index.tsx',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'esbuild-loader',
-        options: {
-          loader: 'tsx',
-          target: 'es2015',
+  return {
+    mode: ENV,
+    entry: './src/index.tsx',
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          loader: 'esbuild-loader',
+          options: {
+            loader: 'tsx',
+            target: 'es2015',
+          },
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        title: `MBC Dev Blog | 기술블로그 | ${ENV === 'developement' && '개발'}`,
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      title: 'MBC Dev Blog | 기술블로그',
-    }),
-  ],
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  devServer: {
-    static: './build',
-    port: 3000,
-    open: true,
-    hot: true,
-  },
-  devtool: 'inline-source-map',
+    optimization: {
+      runtimeChunk: 'single',
+    },
+    devServer: {
+      static: './build',
+      port: 3000,
+      open: true,
+      hot: true,
+    },
+    devtool: 'inline-source-map',
+  };
 };
