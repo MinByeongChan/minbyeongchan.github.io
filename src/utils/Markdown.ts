b19eabd4a5386f2f035847076151b4d7044a7595
MinByeongChan <mbc0481@naver.com>
2022-08-08 22:43:14 +0900

    feature(new-blog): 마크다운 to html 기능 추가

--- src/utils/Markdown.ts
+++ src/utils/Markdown.ts
@@ -1,19 +1,18 @@
-// TODO: @mapbox/rehype-prism does not have typescript definition
-// @ts-ignore
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import gfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
 import rehypePrism from '@mapbox/rehype-prism';
 import html from 'rehype-stringify';
-import gfm from 'remark-gfm';
-import remarkParse from 'remark-parse';
-import remarkRehype from 'remark-rehype';
-import unified from 'unified';
 
-export async function markdownToHtml(markdown: string) {
-  const result = await unified()
+export const getMarkdown = (mdText: string) => {
+  const htmlText = unified()
     .use(remarkParse)
     .use(gfm)
     .use(remarkRehype)
     .use(rehypePrism)
     .use(html)
-    .process(markdown);
-  return result.toString().replace(/@@baseUrl@@/g, process.env.baseUrl || '');
-}
+    .processSync(mdText);
+
+  return String(htmlText);
+};
