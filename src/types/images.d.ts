6ef8aa9563ff7a6a2f39fcea2cbce3d6ab774530
MinByeongChan <mbc0481@naver.com>
2022-08-08 22:42:18 +0900

    feature(new-blog): json, image 파일 읽기 위한 타입 추가

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,4 +1,8 @@
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
-declare module '*.svg';
+// declare module '*.svg';
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
