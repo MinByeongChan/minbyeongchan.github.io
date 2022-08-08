3b7cea66bbe843a6b327de59b19f797b61c85403
MinByeongChan <mbc0481@naver.com>
2022-08-08 22:52:04 +0900

    feature(new-blog): utterances 추가

--- src/components/molecules/comment/Utterances.tsx
+++ src/components/molecules/comment/Utterances.tsx
@@ -1,37 +1,38 @@
-// import React, { createRef, useEffect } from 'react';
+// dependency
+import React, { createRef, useEffect } from 'react';
 
-// const src = 'https://utteranc.es/client.js';
+export interface IUtterancesProps {
+  repo: string;
+  theme: string;
+}
 
-// export interface IUtterancesProps {
-//   repo: string;
-//   theme: string;
-// }
+const src = 'https://utteranc.es/client.js';
 
-// const Utterances: React.FC<IUtterancesProps> = React.memo(({ repo, theme }) => {
-//   const commentRef = createRef<HTMLDivElement>();
+const Utterances = ({ repo, theme }: IUtterancesProps) => {
+  const commentRef = createRef<HTMLDivElement>();
 
-//   useEffect(() => {
-//     const utterances = document.createElement('script');
+  useEffect(() => {
+    const utterances = document.createElement('script');
 
-//     const utterancesConfig = {
-//       src,
-//       repo,
-//       theme,
-//       'issue-term': 'pathname',
-//       crossorigin: 'anonymous',
-//       async: 'true',
-//     };
+    const utterancesConfig = {
+      src,
+      repo,
+      theme,
+      'issue-term': 'pathname',
+      crossorigin: 'anonymous',
+      async: 'true',
+    };
 
-//     Object.entries(utterancesConfig).forEach(([key, value]) => {
-//       utterances.setAttribute(key, value);
-//     });
+    Object.entries(utterancesConfig).forEach(([key, value]) => {
+      utterances.setAttribute(key, value);
+    });
 
-//     commentRef.current!.appendChild(utterances);
-//   }, []);
+    commentRef.current!.appendChild(utterances);
+  }, []);
 
-//   return <div ref={commentRef} />;
-// });
+  return <div ref={commentRef} />;
+};
 
-// Utterances.displayName = 'Utterances';
+Utterances.displayName = 'Utterances';
 
-// export default Utterances;
+export default React.memo(Utterances);
