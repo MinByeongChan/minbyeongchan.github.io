06fb4bf65a69fc67c5aa36688f3c2bfe34bea433
MinByeongChan <mbc0481@naver.com>
2022-08-08 22:41:47 +0900

    feature(new-blog): 프로젝트 메타데이터

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -2,7 +2,7 @@ export const Config = {
   site_name: 'MBC blog',
   title: 'Min Byeong chan',
   description: 'MBC blog',
-  url: 'https://example.com',
+  url: 'https://github.com/MinByeongChan',
   locale: 'kr',
   author: 'Byeong Chan',
   pagination_size: 9,
