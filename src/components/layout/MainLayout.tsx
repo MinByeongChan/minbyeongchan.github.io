060bcd1a711c7407e161bd96ce2191c721feabbc
MinByeongChan <mbc0481@naver.com>
2022-08-08 22:46:29 +0900

    feature(new-blog): 레이아웃 컴포넌트

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,6 +2,7 @@
 import React, { ReactNode } from 'react';
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
+import { fontSize } from '@utils/StyleTheme';
 
 // components
 import Nav from '@components/molecules/Nav';
@@ -9,15 +10,44 @@ import MainBottom from '@components/molecules/MainBottom';
 
 const Main = () => {
   return (
-    <main>
+    <>
       <Nav />
       <LayoutMain>
         <Outlet />
       </LayoutMain>
       <MainBottom />
-    </main>
+    </>
   );
 };
+
+const ResponsiveMainContainer = styled.main`
+  @media (min-width: 500px) and (max-width: 820px) {
+    h2 {
+      font-size: ${fontSize.h3};
+    }
+    h3 {
+      font-size: ${fontSize.xxg};
+    }
+    span,
+    pre,
+    p {
+      font-size: ${fontSize.md};
+    }
+  }
+  @media (min-width: 0px) and (max-width: 499px) {
+    h2 {
+      font-size: ${fontSize.xxg};
+    }
+    h3 {
+      font-size: ${fontSize.xxg};
+    }
+    span,
+    pre,
+    p {
+      font-size: ${fontSize.md};
+    }
+  } ;
+`;
 
 const LayoutMain = styled.section`
   min-height: 650px;
