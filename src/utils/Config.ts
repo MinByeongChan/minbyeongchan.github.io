a8ca16a2d7d2aa6c67a05f7fa3e8ce7aefeb9f05
MinByeongChan <mbc0481@naver.com>
2022-09-04 15:12:07 +0900

    feat(new-blog): firebase config 추가

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -8,3 +8,25 @@ export const Config = {
   pagination_size: 9,
   paging_indicator: 5,
 };
+
+const {
+  FIREBASE_apiKey,
+  FIREBASE_authDomain,
+  FIREBASE_databaseURL,
+  FIREBASE_projectId,
+  FIREBASE_storageBucket,
+  FIREBASE_messagingSenderId,
+  FIREBASE_appId,
+  FIREBASE_measurementId,
+} = process.env;
+
+export const firebaseConfig = {
+  apiKey: FIREBASE_apiKey,
+  authDomain: FIREBASE_authDomain,
+  databaseURL: FIREBASE_databaseURL,
+  projectId: FIREBASE_projectId,
+  storageBucket: FIREBASE_storageBucket,
+  messagingSenderId: FIREBASE_messagingSenderId,
+  appId: FIREBASE_appId,
+  measurementId: FIREBASE_measurementId,
+};
